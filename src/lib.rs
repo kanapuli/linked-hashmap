3be1f2d3372d557237e8ce79c6e5bcdbcb87aed5
puli <athavan@exotel.in>
2019-01-01 07:28:46 -0500

    add HashMap.insert() and Hashmap resize

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use std::collections::hash_map::DefaultHasher;
-use std::hash::Hash;
+use std::hash::{ Hash, Hasher};
+use std::mem;
 const INITIAL_BUCKET_SIZE: usize = 1;
-struct Bucket<K,V>{
-   items: Vec<(K, V)>, 
-}
 pub struct HashMap<K,V > {
-    buckets: Vec<Bucket<K,V>>,
+    buckets: Vec<Vec<(K,V)>>,
+    items: usize,
 }
 
 impl <K, V> HashMap<K,V>
@@ -13,28 +12,35 @@ impl <K, V> HashMap<K,V>
     pub fn new() -> Self{
        HashMap{
             buckets: Vec::new(),
+            items: 0,
         }
     }
 
 }
 
 impl <K, V> HashMap<K,V>
-where: K: Hash 
+where K: Hash  + Eq {
     pub fn insert(&mut self, key: K, value: V) -> Option<V>{
+    
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len()/4 {
+            //if bucket is empty or the item count is three fourth of bucket length
+            //call resize
+            self.resize();
+        }
+
         let mut hasher =  DefaultHasher::new();
         key.hash(&mut hasher);
-        let bucket = hasher.finish() % self.buckets.len();
+        let bucket = (hasher.finish() % self.buckets.len() as u64) as usize ;
         let bucket = &mut self.buckets[bucket];
-        for &mut (ekey, evalue) in  bucket {
+        for &mut (ref ekey,ref mut evalue) in  bucket.iter_mut() {
             //the given key exists already in the hashmap
-            if ekey == key {
-                use std::mem;
+            if ekey == &key {
                 //replace the existing value
                 return Some(mem::replace(evalue, value));
             }
         }
         bucket.push((key,value));
-        
+        None 
     }
 
     fn resize(&mut self){
@@ -42,5 +48,28 @@ where: K: Hash
             0 => INITIAL_BUCKET_SIZE,
             n => 2 * n,
         };
+        let mut new_buckets =  Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new()));
+        for (key, value) in  self
+            .buckets
+            .iter_mut()
+            .flat_map(|bucket| bucket.drain(..)){
+        let mut hasher =  DefaultHasher::new();
+        key.hash(&mut hasher);
+        let bucket = (hasher.finish() % new_buckets.len() as u64) as usize ;
+        new_buckets[bucket].push((key,value));
+        }
+        mem::replace(&mut self.buckets, new_buckets);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn  insert(){
+        let mut map = HashMap::new();
+        map.insert("bar", 96);
     }
 }
