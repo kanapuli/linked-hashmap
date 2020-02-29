0df95c4bbb55fb96da7fa695ed3ff9c673a7cdaf
kanapuli <athavankanapuli@gmail.com>
2020-02-29 23:25:03 +0530

    Add insert method to HashMap
    Make Key of type K to implement Hash trait.
    Add insert() function which hashes the key using the DefaultHasher

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::hash_map::DefaultHasher;
 const INITIAL_NBUCKETS: usize = 1;
 //Bucket is the list of tuples of Key and Values
 struct Bucket<K,V> {
@@ -9,13 +10,25 @@ pub struct HashMap<K,V> {
     buckets: Vec<Bucket<K,V>>
 }
 
-impl<K,V> HashMap<K,V> {
+impl<K,V> HashMap<K,V> 
+where: K: Hash
+{
+
+    //new creates a  new hashMap
     pub fn new() -> Self {
         HashMap{
             buckets: Vec::new(),
         }
     }
+    //insert inserts a bucket into Linked HashMap
+    pub fn insert(&mut self, key: K, value: V){
+        let hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let bucket  = hasher.finish() % self.buckets.len();
+        let bucket = &mut self.buckets[bucket]
 
+    }
+    //resize resizes the HashMaps's bucket length
     fn resize(&mut self){
         let target_size = match self.buckets.len(){
             0 => INITIAL_NBUCKETS,
